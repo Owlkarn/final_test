fd931559bb37108ec79055113cf69bac07a97391
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-09-15 13:33:29 +0500

    Add AddStringToArray metod

--- Program.cs
+++ Program.cs
@@ -7,3 +7,26 @@
     }
     Console.WriteLine();
 }
+string[] AddStringToArray(string[] array)
+{
+    Console.Write("Введите элемент массива (чтобы закончить ввод элементов введите exit): ");
+    string str = Console.ReadLine();
+    int count = 0;
+
+    while (str.ToLower() != "exit")
+    {
+        if (str != String.Empty)
+        {
+            count++;
+            string[] tempArray = new string[count];
+            for (int i = 0; i < count - 1; i++)
+                tempArray[i] = array[i];
+            tempArray[count - 1] = str;
+            array = tempArray;
+        }
+        Console.Write("Введите элемент массива (чтобы закончить ввод элементов введите exit): ");
+        str = Console.ReadLine();
+    }
+    Console.WriteLine();
+    return array;
+}
