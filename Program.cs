55a51eb3d0ccecbaa3c40527e64b8fcea1973401
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-09-15 13:32:38 +0500

    Add PrintArray metod

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿void PrintArray(string[] array)
+{
+    Console.Write("Массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
